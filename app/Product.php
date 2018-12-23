27062a9bcb5b714132d651ac1296033c8da04903
Abdulrahman Faid <abdulrahmanfaid11@gmail.com>
2018-12-23 10:52:17 +0200

    Install spatie/laravel-translatable

--- app/Product.php
+++ app/Product.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Product extends Model
 {
+    use HasTranslations;
+
     protected $guarded = [];
+    public $translatable = ['name', 'description', 'details'];
 }
