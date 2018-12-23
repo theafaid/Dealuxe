27a96e6c8d94321d530c91854e50c6de9dbdd69b
Abdulrahman Faid <abdulrahmanfaid11@gmail.com>
2018-12-23 14:02:18 +0200

    Shop Test

---
+++ tests/Feature/ShopTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ShopTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function a_user_can_browse_shop_page(){
+        $this->get(route('shop.index'))
+            ->assertStatus(200);
+    }
+
+    /** @test */
+    function a_user_must_see_some_products_in_shop_page(){
+        $product1 = factory('App\Product')->create();
+        $product2 = factory('App\Product')->create();
+
+        $this->get(route('shop.index'))
+            ->assertSee($product1->name)
+            ->assertSee($product2);
+    }
+}
