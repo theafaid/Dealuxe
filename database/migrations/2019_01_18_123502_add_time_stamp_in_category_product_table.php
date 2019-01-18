847fd6a5cb5cdff627a94ed5acc34a2245a5ffc7
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-01-18 14:37:48 +0200

    Add timestamps field in category_product table

---
+++ database/migrations/2019_01_18_123502_add_time_stamp_in_category_product_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTimeStampInCategoryProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('category_product', function (Blueprint $table) {
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('category_product', function (Blueprint $table) {
+            $table->timestamps();
+        });
+    }
+}
