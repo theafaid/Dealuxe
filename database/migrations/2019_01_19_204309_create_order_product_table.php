b85e7b0f8394b8e7c2282ed047598a3596676c11
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-01-19 22:50:37 +0200

    Create OrderProduct Model

---
+++ database/migrations/2019_01_19_204309_create_order_product_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('order_id');
+            $table->unsignedInteger('product_id');
+
+            $table->foreign('order_id')->references('id')->on('orders')
+                ->onDelete('set null')->onUpdate('cascade');
+            $table->foreign('product_id')->references('id')->on('products')
+                ->onDelete('set null')->onUpdate('cascade');
+
+            $table->smallInteger('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_products');
+    }
+}
