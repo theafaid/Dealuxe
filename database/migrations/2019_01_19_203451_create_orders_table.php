646ab9ff27d9452919ce3d94e1e9815d73eaa2e4
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-01-19 22:38:43 +0200

    Create a order model to record a order after checkout

---
+++ database/migrations/2019_01_19_203451_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('set null')->onUpdate('cascade');
+
+            $table->string('name_on_card');
+            $table->integer('discount')->default(0);
+            $table->string('coupon_code')->nullable();
+            $table->integer('total');
+            $table->string('payment_gateway')->default('stripe');
+            $table->boolean('shipped')->default(false);
+            $table->string('error')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
